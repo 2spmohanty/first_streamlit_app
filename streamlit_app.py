fa01d03747c3cc48ae2ac6cce09624cac094f44a
Smruti Prakash Mohanty <48411397+2spmohanty@users.noreply.github.com>
2023-09-07 12:04:57 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,8 +40,8 @@ my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 
 my_data_row = my_cur.fetchall()
-streamlit.text("Hello From Snoflake:" )
-streamlit.text(my_data_row)
+streamlit.text("The fruit load list contains:" )
+streamlit.dataframe(my_data_row)
 
 
 
